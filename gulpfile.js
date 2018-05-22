5c07326c873f5fed92b769aad78dd0c54242655a
Xenios Papademetris <xenophon.papademetris@yale.edu>
2018-05-21 21:43:33 -0400

    eslint fix

--- gulpfile.js
+++ gulpfile.js
@@ -246,16 +246,30 @@ var esLint=function() {
     // eslint() attaches the lint output to the "eslint" property
     // of the file object so it can be used by other modules.
         .pipe(eslint({
-            globals: [
-                'jQuery',
-                '$'
-            ],
-        }))
-    // eslint.format() outputs the lint results to the console.
-    // Alternatively use eslint.formatEach() (see Docs).
-        .pipe(eslint.format());
-    // To have the process exit with an error code (1) on
-    // lint error, return the stream and pipe to failAfterError last.
+            "env": {
+                "browser": true,
+                "node": true,
+                "commonjs": true,
+                "es6": true
+            },
+            "extends": "eslint:recommended",
+            "parserOptions": {
+                "sourceType": "module",
+                "ecmaVersion": 2017
+            },
+            "rules": {
+                "linebreak-style": [
+                    "error",
+                    "unix"
+                ],
+                'no-console': 'off',
+                'indent' : 'off',
+                "semi": [
+                    "error",
+                    "always"
+                ]
+            }
+        })).pipe(eslint.format());
 };
 
 gulp.task('eslint', () => {
