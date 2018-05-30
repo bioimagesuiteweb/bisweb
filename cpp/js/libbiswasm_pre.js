68def5597f8d1bc66e2264ccfdbd7f5810453157
Xenios Papademetris <xenophon.papademetris@yale.edu>
2018-05-30 16:45:23 -0400

    GROwABLE MEMORY, reduce initial allocation

--- cpp/js/libbiswasm_pre.js
+++ cpp/js/libbiswasm_pre.js
@@ -6,6 +6,12 @@
         mode = mode || 'node.js'
 	binary = binary || false;
 	var Module = { };
+
+        // Memory can be allocated here if we prefer
+        let psize = 64*1024;
+        let tm = 16*1024*1024;
+        let fm = 2048*1024*1024;
+        Module['wasmMemory'] = new WebAssembly.Memory({ 'initial': tm / psize, 'maximum': fm / psize });
         
 	Module['onRuntimeInitialized'] = function() {
 	    const usingWasm =  Module["usingWasm"] || false;
@@ -13,6 +19,7 @@
 		console.log('==== \tModule loaded, ',mode,' usingWasm='+usingWasm.toString()+' wasmname='+wasmname);
 	    else
 		console.log('==== \tModule ready,',mode,' usingWasm='+usingWasm.toString()+' preloaded from '+wasmname);
+//            console.log('Memory size=',Module['wasmMemory'].buffer.byteLength/(1024*1024),' MB');
 	    callback(Module);
 	};
 
