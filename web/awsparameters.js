972dca336e2c01b1bcf86c4517bafbc23bb9c316
Zach S <ZaSaltzman@gmail.com>
2018-07-11 14:47:21 -0400

    Moved aws parameters to their own file

---
+++ web/awsparameters.js
@@ -0,0 +1,10 @@
+
+let awsparams = {
+    'ClientId': '5edh465pitl9rb04qbi37csv8e',
+    'AppWebDomain': 'bisweb-test.auth.us-east-1.amazoncognito.com',
+    'TokenScopesArray': ['email', 'openid'],
+    'RedirectUriSignIn': 'http://localhost:8080/web/biswebaws.html',
+    'RedirectUriSignOut': 'http://localhost:8080/web/biswebaws.html',
+    'IdentityProvider': 'COGNITO',
+    'UserPoolId': 'us-east-1_BAOsizFzq'
+};
