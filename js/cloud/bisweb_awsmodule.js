f42dd860a34eef43c0727cf1955f27ef16f2dbb2
Zach S <ZaSaltzman@gmail.com>
2018-07-11 14:27:19 -0400

    Added amazon aws authorization

--- js/cloud/bisweb_awsmodule.js
+++ js/cloud/bisweb_awsmodule.js
@@ -16,7 +16,12 @@ class AWSModule {
         //data related to Amazon AWS services
         this.bucketName = 'bisweb-test';
         this.regionName = 'us-east-1'; //N. Virginia
+
+        const userPool = 'us-east-1_BAOsizFzq';
         const identityPool = 'us-east-1:13a0bffd-384b-43d8-83c3-050815009aa6';
+        const clientId = '5edh465pitl9rb04qbi37csv8e';
+        const redirectURI = 'http://localhost:8080/web/biswebaws.html';
+
         AWS.config.update({
             'region' : this.regionName,
             'credentials' : new AWS.CognitoIdentityCredentials({
@@ -27,24 +32,36 @@ class AWSModule {
         //AWSCognitoIdentity.config.region = this.regionName;
 
         const userPoolData = {
-            'UserPoolId' : 'us-east-1_BAOsizFzq',
-            'ClientId' : '5edh465pitl9rb04qbi37csv8e'
+            'UserPoolId' : userPool,
+            'ClientId' : clientId
         };
 
-        console.log('cognito identity', AWSCognitoIdentity);
         this.userPool = new AWSCognitoIdentity.CognitoUserPool(userPoolData);
         this.userData = {
             'username' : null,
             'pool' : null
         };
 
+        this.authData = {
+            'ClientId' : clientId,
+            'AppWebDomain' : 'bisweb-test.auth.us-east-1.amazoncognito.com',
+            'TokenScopesArray' : [ 'email', 'openid' ],
+            'RedirectUriSignIn' : redirectURI,
+            'RedirectUriSignOut' : redirectURI,
+            'IdentityProvider' : 'COGNITO', 
+            'UserPoolId' : userPool
+        };
+
+        this.awsAuth = null
         this.s3 = this.createS3(this.bucketName);
         this.listObjectsInBucket();
+
+        //set to the values provided by Cognito when the user signs in
+        this.cognitoUser = null;
 
         //UI features
         this.createUserModal = null;
         this.authUserModal = null;
-
     }
 
     createS3(bucketName) {
@@ -85,9 +102,9 @@ class AWSModule {
                     for (let i = 0; i < xmlRequest.response.length; i++) {
                         bufferView[i] = xmlRequest.response.charCodeAt(i);
                     }
-
-                    let unzippedFile = wsutil.unzipFile(bufferView);
-                    console.log('bufferView', bufferView, 'buffer', buffer, 'unzipped file', unzippedFile);
+                    
+                    let unzippedFile = wsutil.unzipFile(bufferView);awsAuth
+                    console.log('bufferView', bufferView, 'buffer', buffer, 'unzipped file', unawsAuthzippedFile);
 
                     let parsedImage = new bisweb_image();
                     parsedImage.parseNII(unzippedFile);
@@ -199,8 +216,33 @@ class AWSModule {
     }
 
     wrapInAuth(command, parameters = null) {
-        console.log('TODO: implement wrap in auth....');
+
+        if (!this.cognitoUser) {
+            this.awsAuthUser();
+            return;
+        }
+
+        switch(command) {
+            case 'showfiles' : this.listFilesInBucket(); break;
+            case 'uploadfile' : this.uploadFileToBucket(parameters); break;
+            default : console.log('Unrecognized aws command', command, 'cannot complete request.');
+        }
         console.log('called command', command, 'with parameters', parameters);
+    }
+
+    awsAuthUser() {
+        let authPage = window.open('../web/biswebaws.html', '_blank', 'width=400, height=400');
+        let authData = this.authData;
+        /*$(authPage).ready( function() {
+            let auth = new AWSCognitoAuth.CognitoAuth(authData);
+            auth.userhandler = {
+                onSuccess: () => { console.log('logged in successfully!') },
+                onFailure: () => { console.log('failed to login'); }
+            }
+            console.log('window', window);
+            console.log('auth', auth);
+            //auth.getSession();
+        });*/
     }
 }
 
