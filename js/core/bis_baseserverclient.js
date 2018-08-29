f1f5ee81f3aa701bc9304be431e7874d11075f33
Xenios Papademetris <xenophon.papademetris@yale.edu>
2018-08-29 12:18:13 -0400

    create base server client

---
+++ js/core/bis_baseserverclient.js
@@ -0,0 +1,92 @@
+
+/**
+  * This is an abstract base class for a server client 
+  * It is extended for both the BioImage Suite File Server Client
+  *    and the AWS Client
+*/
+
+class BisBaseServerClient { 
+
+    constructor() {
+        this.authenticated = false;
+        this.hasGUI=false;
+    }
+
+    /** GUI Replacement for webutil.createAlert */
+    alertEvent(name,error=false) {
+        if (error)
+            console.log('---- File Client Error:',name);
+        else
+            console.log('____ File Client:',name);
+    }
+
+    /** Pure Virtual to be replaced for GUI */
+    retryAuthenticationDialog() { }
+    showAuthenticationDialog() { }
+    hideAuthenticationDialog() { }
+    showFileDialog() {}
+
+
+    // ------------------ Download file and helper routines -----------------------------------------------------------------
+    /**
+     * downloads a file from the server 
+     * @param{String} url - the filename
+     * @param{Boolean} isbinary - if true file is binary
+     * @returns a Promise with payload { obj.name obj.data } much like bis_genericio.read (from where it will be called indirectly)
+     */
+    downloadFile(url,isbinary) {
+        return Promise.rejected('downloadFile not implemented '+url+' '+isbinary);
+    }
+    
+    /** upload file 
+     * @param {String} url -- abstact file handle object
+     * @param {Data} data -- the data to save, either a sting or a Uint8Array
+     * @param {Boolean} isbinary -- is data binary
+     * @returns {Promise} 
+     */
+    uploadFile(url, data, isbinary=false) {
+        return Promise.rejected('uploadFile not implemented '+url+' '+isbinary+' ' +typeof data);
+    }
+
+    /** get file size -- the size of a file
+     * @param{String} url -- the filename
+     * @returns {Promise} payload is the file size
+     */
+    getFileSize(url) {
+        return Promise.rejected('getFileSize not implemented '+url);
+    }
+
+    /** checks is filename is a directory
+     * @param{String} url -- the filename
+     * @returns {Promise} payload true or false
+     */
+    isDirectory(url) {
+        return Promise.rejected('isDirectory not implemented '+url);
+    }
+
+    /** creates a directory
+     * @param{String} url -- the directory name
+     * @returns {Promise} payload true (created) or false (already existing)
+     */
+    makeDirectory(url) {
+        return Promise.rejected('makeDirectory not implemented '+url);
+    }
+
+    /** deletes a directory
+     * @param{String} url -- the directory name
+     * @returns {Promise} payload true or false
+     */
+    deleteDirectory(url) {
+        return Promise.rejected('deleteDirectory not implemented '+url);
+    }
+
+    /** getMatching Files
+     * @param{String} querystring -- the matching string
+     * @returns {Promise} payload list of filenames that match
+     */
+    getMatchingFiles(querystring) {
+        return Promise.rejected('getMatchingFiles not implemented '+querystring);
+    }
+}
+
+module.exports = BisBaseServerClient;
