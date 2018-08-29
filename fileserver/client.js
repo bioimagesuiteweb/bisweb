8846dacdcb68ee984ef9d3799470e951d6441a3d
Xenios Papademetris <xenophon.papademetris@yale.edu>
2018-08-29 13:09:05 -0400

    implemented filesystem operations

--- fileserver/client.js
+++ fileserver/client.js
@@ -33,22 +33,45 @@ let p=async function() {
     let t=await client.getServerTempDirectory();
     console.log('\nTemp =',t);
 
+    let newdirname=`${bd}/tmp_xenios`;
+    try {
+        let f1=await genericio.getFileSize(`${basedir}/MNI_T1_2mm_stripped_ras.nii.gz`);
+        console.log('File size=',f1);
+        
+        let f2=await genericio.isDirectory(basedir);
+        console.log('Basedir is directory=',f2);
+        
+        let f3=await genericio.isDirectory(`${basedir}/MNI_T1_2mm_stripped_ras.nii.gz`);
+        console.log('is File Directory=',f3);
+        
 
+        let f4=await genericio.makeDirectory(newdirname);
+        console.log('made File Directory=',newdirname,f4);
+
+        let f6=await genericio.isDirectory(newdirname);
+        console.log(newdirname,' is directory=',f6);
+        
+        
+    } catch(e) {
+        console.log(e);
+    }
+
+    
     console.log('++++\n++++ Download File\n++++');
     let img=new BisWebImage();
     await img.load(`${basedir}/MNI_T1_2mm_stripped_ras.nii.gz`,'None');
     console.log('Done ', img.getDescription());
 
-    
+    let maxk=40;
 
-    for (let k=0;k<20;k++) {
+    for (let k=1;k<maxk;k+=5) {
         try {
-            let f=await img.save(`${bd}/t.nii.gz`);
+            let fname=`${newdirname}/t${k}.nii.gz`;
+            let f=await img.save(fname);
             console.log('\n Saved =',JSON.stringify(f));
             
-            
             let img2=new BisWebImage();
-            await img2.load(`${bd}/t.nii.gz`);
+            await img2.load(fname);
             
             console.log("Read",img2.getDescription());
             
@@ -61,6 +84,23 @@ let p=async function() {
         }
     }
 
+    console.log('++++\n++++ File List\n++++');
+    let p0=await client.requestFileList('load',newdirname);
+    console.log('\n',JSON.stringify(p0));
+
+    let query=newdirname+'/*1*.nii.gz';
+    console.log('---------------------------------------------',query);
+    let p00=await genericio.getMatchingFiles(query);
+    console.log('\n matched ',query,'=',JSON.stringify(p00));
+
+    try {
+        let f5=await genericio.deleteDirectory(newdirname);
+        console.log('deleted Directory=',newdirname,f5);
+    } catch(e) {
+        console.log('Failed to delete Directory',newdirname,e);
+    }
+
+    
     process.exit();
 };
 
