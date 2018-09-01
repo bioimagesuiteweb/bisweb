dbf54b377c6447be9d144bbb69603a7272a7902b
Xenios Papademetris <xpapademetris@gmail.com>
2018-09-01 14:39:46 -0400

    added server to path

--- config/setpaths.sh
+++ config/setpaths.sh
@@ -1,5 +1,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export PATH=${DIR}/bin:${DIR}/python/modules:${PATH}
+export PATH=${DIR}/bin:${DIR}/server:${DIR}/python/modules:${PATH}
 
 echo "------------------------------------------------------------------------------------"
 echo "BISWEB scripts are now in your path"
