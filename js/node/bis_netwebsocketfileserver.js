7d8b3717d7831b699d79b8d88765d833a3054dd1
Xenios Papademetris <xpapademetris@gmail.com>
2018-09-04 20:58:23 -0400

    Copied code from utility files in here as this is heading towards obsolesence

--- js/node/bis_netwebsocketfileserver.js
+++ js/node/bis_netwebsocketfileserver.js
@@ -6,11 +6,6 @@ const timers = require('timers');
 const util = require('bis_util');
 const { StringDecoder } = require('string_decoder');
 
-const bisserverutil=require('bis_fileserverutils');
-const formatPacket=bisserverutil.formatPacket;
-const readFrame=bisserverutil.readFrame;
-
-
 // TODO: IP Filtering
 // TODO: Check Base Directories not / /usr (probably two levels)
 
@@ -26,6 +21,168 @@ const globalInitialServerPort=wsutil.initialPort;
 
 
 const BaseFileServer=require('bis_basefileserver');
+
+
+// -----------------------------------------------------------------------------
+//
+// Utility Functions
+//
+// -----------------------------------------------------------------------------
+/**
+ * Takes an opcode and a payload length and makes a WebSocket control frame. 
+ * @param {Number} opcode - Opcode for the data transmission (see documentation for more details)
+ * @param {Number} payloadLength - Size of the payload, excluding the size of the control frame
+ */
+let formatControlFrame = (opcode, payloadLength) => {
+    let controlFrame;
+    if (payloadLength < 126) {
+        controlFrame = new Uint8Array(2);
+        controlFrame[1] = payloadLength;
+    } else if (payloadLength < 65536) {
+        controlFrame = new Uint8Array(4);
+        controlFrame[1] = 126;
+        controlFrame[2] = payloadLength / 256;
+        controlFrame[3] = payloadLength % 256;
+    } else {
+        controlFrame = new Uint8Array(10);
+        let remainingPayload = payloadLength;
+        controlFrame[1] = 127;
+        controlFrame[2] = Math.floor(remainingPayload / Math.pow(256, 7)); remainingPayload = remainingPayload - controlFrame[2] * Math.pow(256, 7);
+        controlFrame[3] = Math.floor(remainingPayload / Math.pow(256, 6)); remainingPayload = remainingPayload - controlFrame[3] * Math.pow(256, 6);
+        controlFrame[4] = Math.floor(remainingPayload / Math.pow(256, 5)); remainingPayload = remainingPayload - controlFrame[4] * Math.pow(256, 5);
+        controlFrame[5] = Math.floor(remainingPayload / Math.pow(256, 4)); remainingPayload = remainingPayload - controlFrame[5] * Math.pow(256, 4);
+        controlFrame[6] = Math.floor(remainingPayload / Math.pow(256, 3)); remainingPayload = remainingPayload - controlFrame[6] * Math.pow(256, 3);
+        controlFrame[7] = Math.floor(remainingPayload / Math.pow(256, 2)); remainingPayload = remainingPayload - controlFrame[7] * Math.pow(256, 2);
+        controlFrame[8] = Math.floor(remainingPayload / Math.pow(256, 1)); remainingPayload = remainingPayload - controlFrame[8] * Math.pow(256, 1);
+        controlFrame[9] = remainingPayload;
+    }
+
+    //TODO: implement logic for setting fin bit
+    controlFrame[0] = opcode;
+    controlFrame[0] = controlFrame[0] | 0b10000000;
+
+    return controlFrame;
+};
+
+/**
+ * Takes a payload and a description of the payload type and formats the packet for transmission. 
+ * 
+ * @param {String} payloadType - A word describing the nature of the payload.
+ * @param {String|Binary} data - The payload for the packet.
+ * @returns A raw bytestream that can be sent over the socket.
+ */
+let formatPacket = function(payloadType, data) {
+    let payload, opcode;
+    //transmissions are either text (JSON) or a raw image 
+    if (payloadType === 'binary') {
+        payload = data;
+        opcode = 2;
+    } else {
+        payload = JSON.stringify({
+            'type' : payloadType,
+            'payload' : data
+        });
+        opcode = 1;
+        //console.log('Sending payload',payload.substr(0,100));
+    }
+    
+    let controlFrame = formatControlFrame(opcode, payload.length);
+    let packetHeader = Buffer.from(controlFrame.buffer), packetBody = Buffer.from(payload);
+    let packet = Buffer.concat([packetHeader, packetBody]);
+    
+    return packet;
+};
+
+
+const readFrame = function(chunk) {
+        
+    let controlFrame = chunk.slice(0, 14);
+    let parsedControl = parseControlFrame(controlFrame);
+    //    console.log('parsed control frame', parsedControl);
+    
+    //drop unmasked packets
+    if (!parsedControl.mask) {
+        //console.log('Received a transmission with no mask from client, dropping packet.'); 
+        return;
+    }
+    
+    if (parsedControl.payloadLength<0 ||
+        parsedControl.payloadLength>65536) {
+        //console.log('Chunk=',chunk.byteLength);
+        //        console.log('ControlFrame=',controlFrame,controlFrame.byteLength);
+        //        console.log('Bad payload',parsedControl.payloadLength);
+        return;
+    }
+    
+    
+    let decoded = new Uint8Array(parsedControl.payloadLength);
+    
+    //decode the raw data (undo the XOR)
+    for (let i = 0; i < parsedControl.payloadLength; i++) {
+        decoded[i] = chunk[i + parsedControl.datastart] ^ parsedControl.mask[i % 4];
+    }
+    
+    return { 
+        'parsedControl' : parsedControl,
+        'decoded' : decoded
+    };
+};
+
+/**
+ * Parses the first 112 bits of a WebSocket dataframe, i.e. the control portion of the frame.
+ * https://developer.mozilla.org/en-US/docs/Web/API/WebSockets_API/Writing_WebSocket_servers#Exchanging_Data_frames
+ * @param {Uint8Array} frame 
+ * @return An dictionary of separated control frame values
+ */
+let parseControlFrame = (frame) => {
+    let fin = frame[0] >> 7;
+    let opcode = frame[0] % 16;
+    let maskbit = frame[1] >> 7;
+    let payload = frame[1] % 128;
+    let maskkey = maskbit ? frame.slice(2, 6) : null;
+    let datastart = maskbit ? 6 : 2;
+
+    if (payload === 126) {
+        payload = frame[2] * 256 + frame[3];
+        maskkey = maskbit ? frame.slice(4, 8) : null;
+        datastart = maskbit ? 8 : 4;
+    }
+
+    if (payload === 127) {
+        payload = frame[2] * Math.pow(256, 7) + frame[3] * Math.pow(256, 6) + frame[4] * Math.pow(256, 5) + frame[5] * Math.pow(256, 4)
+            + frame[6] * Math.pow(256, 3) + frame[7] * Math.pow(256, 2) + frame[8] * Math.pow(256, 1) + frame[9] * Math.pow(256, 0);
+        maskkey = maskbit ? frame.slice(10, 14) : null;
+        datastart = maskbit ? 14 : 10;
+    }
+
+    return {
+        'fin': fin,
+        'opcode': opcode,
+        'maskbit': maskbit,
+        'payloadLength': payload,
+        'mask': maskkey,
+        'datastart': datastart
+    };
+};
+
+
+/**
+ * Decodes series of raw UTF-8 characters, i.e. numbers, into something human readable.
+ * 
+ * @param {Uint8Array} rawText - Series of raw UTF-8 characters
+ * @param {Object|Number} control - Parsed control frame (see parseControlFrame), or length of payload.
+ * @return Decoded string
+ */
+let decodeUTF8 = (rawText, control) => {
+    let payloadLength = typeof (control) === 'object' ? control.payloadLength : control;
+    let text = "";
+    //decode from raw UTF-8 values to characters
+    for (let i = 0; i < payloadLength; i++) {
+        text = text + String.fromCharCode(rawText[i]);
+    }
+
+    return text;
+};
 
 
 // .................................................. This is the class ........................................
@@ -148,7 +305,7 @@ class BisNetWebSocketFileServer extends BaseFileServer {
      * @returns {String} - the decoded string
      */
     decodeUTF8(text,length) {
-        return wsutil.decodeUTF8(text, length);
+        return decodeUTF8(text, length);
     }
     // --------------------------------------------------------------------------
 
@@ -307,7 +464,7 @@ class BisNetWebSocketFileServer extends BaseFileServer {
         let readOTP = (chunk) => {
             let frame = readFrame(chunk);
             let decoded = frame.decoded;
-            let password = wsutil.decodeUTF8(decoded, frame.parsedControl);
+            let password = decodeUTF8(decoded, frame.parsedControl);
             
             console.log('..... password sent by client=('+password+')');
 
@@ -391,7 +548,7 @@ class BisNetWebSocketFileServer extends BaseFileServer {
         this.attachSocketEvent(socket,'data', (chunk) => {
             
             let controlFrame = chunk.slice(0, 14);
-            let parsedControl = wsutil.parseControlFrame(controlFrame);
+            let parsedControl = parseControlFrame(controlFrame);
             //        console.log('.....','parsed control frame', parsedControl);
             
             if (!parsedControl.mask) {
