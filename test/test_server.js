b6926cd41514cfb1b26302a798d903da2b15c187
Xenios Papademetris <xenophon.papademetris@yale.edu>
2018-09-27 09:35:03 -0400

    updated version and tweaked deface

--- test/test_server.js
+++ test/test_server.js
@@ -260,20 +260,6 @@ describe('Testing the WS server\n', function() {
         });
     });
 
-    it('WS ...test dummy dcm2nii conversion',function(done) {
-
-        let hello = function(msg) {
-            console.log(' ____'+msg+'_____\n');
-        };
-
-        
-        
-        client.dicomConversion(__dirname,hello,true).then( (m) => {
-            console.log('All set ',m);
-            assert(true,true);
-            done();
-        });
-    });
 
     after(function(done) {
         bisserverutil.terminateTestingServer(client).then( ()=> {
