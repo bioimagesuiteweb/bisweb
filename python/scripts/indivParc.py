7281e05c760e1089a636dcb70cd45ce439aac8bd
Mehraveh Salehi <mehravehs@gmail.com>
2019-05-17 16:50:55 -0400

    test

--- python/scripts/indivParc.py
+++ python/scripts/indivParc.py
@@ -1,4 +1,5 @@
 # LICENSE
+
 # 
 # _This file is Copyright 2018 by the Image Processing and Analysis Group (BioImage Suite Team). Dept. of Radiology & Biomedical Imaging, Yale School of Medicine._
 # 
