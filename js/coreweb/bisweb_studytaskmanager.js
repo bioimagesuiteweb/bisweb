aa0980138665ca437469079dd28138f27811b377
Xenios Papademetris <xenophon.papademetris@yale.edu>
2019-06-14 10:53:40 -0400

    updates

--- js/coreweb/bisweb_studytaskmanager.js
+++ js/coreweb/bisweb_studytaskmanager.js
@@ -2,8 +2,32 @@ const webutil = require('bis_webutil.js');
 const bis_webfileutil = require('bis_webfileutil.js');
 const bis_bidsutils = require('bis_bidsutils.js');
 const bisweb_taskutils = require('bisweb_taskutils.js');
-
 const bootbox = require('bootbox');
+const $ = require('jquery');
+const util=require('bis_util');
+
+const OFFSET=20;
+const AXIS=40;
+const OPACITY=0.5;
+
+const dummydata=`{
+    "units": "frames",
+    "TR": 1,
+    "offset": 0,
+    "runs" : {
+        "run1": {
+            "motor": "0-20",
+            "visual": "40-60",
+            "auditory": "80-99"
+        },
+        "run2": {
+            "motor":    [ "0-10",  "40-51", "75-80" ],
+            "visual":   [ "12-22", "53-62", "82-83" ],
+            "auditory": [ "25-37", "66-72", "85-98" ]
+        }
+    }
+}`;
+
 
 
 class StudyTaskManager {
@@ -15,7 +39,14 @@ class StudyTaskManager {
         this.studypanel=studypanel;
         this.taskplotter=null;
         this.viewerid=viewerid;
+        this.viewer=document.querySelector(this.viewerid);
         this.taskdata=null;
+        this.graphWindow=null;
+        this.dialogElement=null;
+        this.canvas=null;
+        this.legendMargin=0;
+        this.margins=[];
+        this.htask=0;
         console.log('Task Manager created');
     }
 
@@ -155,7 +186,14 @@ class StudyTaskManager {
                 'filters' : 'DIRECTORY',
                 'save' : false
             }
-        );*/
+            );*/
+
+        //XP Change
+        if (dummydata) {
+            this.taskdata= bisweb_taskutils.parseData(dummydata);
+            this.plotTaskData();
+        }
+            
     }
 
     /**
@@ -188,78 +226,7 @@ class StudyTaskManager {
             this.plotTaskData();
     }
         
-    
-    plotTaskData() {
-        
-        if (this.taskdata===null) {
-            webutil.createAlert('No task definitions in memory',true);
-            return;
-        }
-
-        if (this.taskplotter===null) {
-            this.taskplotter = document.createElement('bisweb-taskplotterelement');
-            this.taskplotter.setAttribute('bis-viewerid',this.viewerid);
-            document.body.appendChild(this.taskplotter);
-        }
-        
-        //parse ranges into 0 and 1 array
-        let parsedRuns = this.taskdata.runs;
-        let taskNames = this.taskdata.taskNames;
-        let taskCharts = {};
-
-        console.log('parsedRuns',parsedRuns);
-        const step=(100.0/(taskNames.length+1));
-        const scale=0.8*step;
-                
-        let runKeys=Object.keys(parsedRuns);
-
-        for (let i=0;i<runKeys.length;i++) {
-
-            let regionsArray = [];
-            let regions = {};        
-            let runkey=runKeys[i];
-            for (let t=0;t<taskNames.length;t++) {
-                let taskKey=taskNames[t];
-                let base=t*step;
-                
-                if (parsedRuns[runkey].parsedRegions[taskKey]) {
-
-                    let ukey=taskKey;
-                    let tmp=parsedRuns[runkey].parsedRegions[taskKey];
-                    regions[ukey] = new Array(tmp.length*3);
-                    for (let j=0;j<tmp.length;j++) {
-                        tmp[j]=tmp[j]*scale+base;
-                        for (let ia=0;ia<=2;ia++)
-                            regions[ukey][j*3+ia]=tmp[j];
-                    }
-                }
-            }
-
-            let labelsArray = Object.keys(regions).sort();
-            for (let ia = 0; ia < labelsArray.length; ia++) 
-                regionsArray.push(regions[labelsArray[ia]]);
-            taskCharts[runkey] = this.taskplotter.formatChartData(regionsArray,
-                                                                  new Array(labelsArray.length).fill(1),
-                                                                  labelsArray,1.0/3.0);
-        }
-        //let n='All Tasks';
-        //taskCharts[n] = blockChart;
-        let name=runKeys[0];
-        
-        this.taskplotter.createChart({
-            'xaxisLabel': 'Time',
-            'yaxisLabel': 'Activation',
-            'isFrameChart': false,
-            'charts': taskCharts,
-            'makeTaskChart': false,
-            'displayChart': name,
-            'chartType': 'line',
-        });
-
-        return Promise.resolve();
-    }
-
-
+   
     createTSVParseModal(f) {
         bootbox.confirm({
             'message' : 'Overwrite any existing .tsv files with ones parsed from ' + f + '?',
@@ -283,6 +250,304 @@ class StudyTaskManager {
             }
         });
     }
+
+    // -----------------------------------------------------------------------------------
+    createTaskPlotWindow() {
+
+        if (this.graphWindow) {
+            this.graphWindow.cleanup();
+        }
+        
+        let dim= this.viewer.getViewerDimensions();
+        
+        //search HTML for a dock open on the left
+        //if it exists, we want to make sure the graph is displayed over it so we add extra width
+        let docks = $('.biswebdock');
+        for (let dock of docks) {
+            if ($(dock).css('left') === '0px') {
+                dim[0] += parseInt($(dock).css('width'));
+            }
+        }
+        
+        let width = dim[0] - 20;
+        let height = dim[1] * 0.85;
+        let left = 10;
+        let top = 40;
+        
+        this.dialogElement=document.createElement('bisweb-dialogelement');
+        console.log('Dlg=',this.dialogElement);
+        let graphWindow =this.dialogElement.create('Task Plotter', width, height, left, top, 200, false);
+        
+        let innerh = height - 120;
+        let innerw = width - 10;
+        
+        graphWindow.widget.css({
+            'margin': '0 0 0 0',
+            'padding': '0 0 0 0',
+            'height': `${innerh}px`,
+            'width': `${innerw}px`,
+            "overflow-y": "hidden",
+            "overflow-x": "hidden",
+        });
+        
+        graphWindow.widgetbase.css({
+            'height': `${innerh}px`,
+            'width': `${innerw}px`,
+            'background-color': '#222222',
+            'margin': '0 0 0 0',
+            'padding': '0 0 0 0',
+            "overflow-y": "hidden",
+            "overflow-x": "hidden",
+        });
+        
+        graphWindow.widget.empty();
+        this.graphWindow=graphWindow;
+        let dm=[innerw, innerh - 15];
+        this.graphcanvasid = webutil.getuniqueid();
+        
+        graphWindow.widget.css({ "background-color": "#222222" });
+        graphWindow.setCloseCallback(() => {
+            graphWindow.hide();
+        });
+
+        let cw = dm[0];
+        let ch = dm[1];
+
+        this.canvas = document.createElement('canvas');
+        this.canvas.width=cw;
+        this.canvas.height=ch;
+        graphWindow.widget.append($(this.canvas));
+        console.log('Canvas=',this.canvas);
+
+        this.dialogElement.removeCloseButton();
+        let footer=this.dialogElement.getFooter();
+        footer.css({
+            "height" : "50px",
+            'margin' : '3 3 3 3',
+            'padding' : '0 0 0 0',
+            "overflow-y": "hidden",
+            "overflow-x": "hidden" ,
+        });
+        footer.empty();
+        graphWindow.show();
+    }
+
+
+    /** Initialize the plot and then plot the first run */
+    plotTaskData() {
+        
+        this.createTaskPlotWindow();
+
+        if (this.taskdata===null) {
+            webutil.createAlert('No task definitions in memory',true);
+            return;
+        }
+
+        const taskNames = this.taskdata.taskNames;
+        const footer=this.dialogElement.getFooter();
+        const parsedRuns = this.taskdata.runs;
+        const runKeys=Object.keys(parsedRuns);
+        
+        for (let i=0;i<runKeys.length;i++) {
+            let runName=runKeys[i];
+            this.addItemToFooter(footer,runName);
+        }
+
+        
+        let canvas=this.canvas;
+        let context=canvas.getContext("2d");
+        context.clearRect(0,0,canvas.width,canvas.height);
+        context.fillStyle="#444444";
+        context.fillRect(0,0,canvas.width,canvas.height);
+        
+        context.font='14px Arial';
+        context.fillStyle = "#ffffff";
+
+        const textw=120,texth=25,offset=20;
+        this.legendMargin=Math.ceil(textw+offset);
+
+        console.log('Canvas=',canvas.width,canvas.height,'margin=',this.legendMargin);
+
+        this.margins=[OFFSET,OFFSET,(canvas.width-this.legendMargin-OFFSET),canvas.height-2*OFFSET ];
+        this.htask=Math.floor((this.margins[3]-1.1*AXIS)/taskNames.length);
+        
+        for (let i=0;i<taskNames.length;i++) {
+            let y=50+i*this.htask;
+            let x=Math.round(canvas.width-this.legendMargin+offset/2);
+            let cl=util.objectmapcolormap[i+1];
+            context.fillStyle=`rgba(${cl[0]},${cl[1]},${cl[2]},${OPACITY})`;
+            context.fillRect(x,y,textw,1.25*texth);
+            context.fillStyle="#000000";
+            context.fillText(taskNames[i],Math.floor(x+offset/2),Math.floor(y+texth));
+        }
+
+        
+        this.plotTaskDataRun(runKeys[0]);
+    }
+
+    /** Plot data for a single run */
+    plotTaskDataRun(runName) {
+
+        this.dialogElement.setTitle('Showing Task Definitions for '+runName.toUpperCase());
+        
+        let canvas=this.canvas;
+        let context=canvas.getContext("2d");
+        
+        context.fillStyle="#383838";
+        context.fillRect(this.margins[0],this.margins[1],this.margins[2],this.margins[3]);
+
+        const taskNames = this.taskdata.taskNames;
+        const parsedRuns = this.taskdata.runs;
+        const runInfo=parsedRuns[runName];
+
+        let taskbase=this.margins[0];
+        
+        let maxt=0;
+        for (let i=0;i<taskNames.length;i++) {
+            let task=taskNames[i];
+            let runpairs=runInfo[task];
+            // TODO: Fix this in parsing runpairs SHOULD ALWAYS by an array of arrays!
+            if (typeof runpairs[0] === "number")
+                runpairs=[runpairs]; 
+
+            for (let i=0;i<runpairs.length;i++) {
+                let m=parseFloat(runpairs[i][1]);
+                if (m>maxt)
+                    maxt=m;
+            }
+        }
+        maxt=Math.ceil(maxt/20)*20;
+
+        // draw Time axis;
+        let axis_start=this.margins[0]+OFFSET;
+        let axis_stop=this.margins[0]+this.margins[2]-2*OFFSET;
+        context.beginPath();
+        let liney=this.margins[1]+this.margins[3]-0.7*AXIS;
+        context.strokeStyle="#ffffff";
+        console.log('axis=',axis_start,axis_stop,liney,this.margins);
+        context.moveTo(axis_start,liney);
+        context.lineTo(axis_stop,liney);
+        context.stroke();
+        context.font='12px Arial';
+        context.fillStyle = "#ffffff";
+        context.textAlign="center";
+        context.textBaseline="bottom";
+
+
+        for (let i=0;i<taskNames.length;i++) {
+            let task=taskNames[i];
+            let runpairs=runInfo[task];
+            // TODO: Fix this in parsing runpairs SHOULD ALWAYS by an array of arrays!
+            if (typeof runpairs[0] === "number")
+                runpairs=[runpairs];
+            let maxy=taskbase+this.htask*(i+1);
+            let miny=maxy-0.8*this.htask;
+            
+            context.save();
+            context.strokeStyle="#cccccc";
+            context.beginPath();
+            context.setLineDash([20,5]);
+            context.moveTo(axis_start,maxy);
+            context.lineTo(axis_stop,maxy);
+            context.stroke();
+            context.restore();
+            context.save();
+            let cl=util.objectmapcolormap[i+1];
+            for (let i=0;i<runpairs.length;i++) {
+                let limits=[ runpairs[i][0], runpairs[i][1] ];
+                console.log('Draing',task,limits,cl);
+                for (let i=0;i<=1;i++) {
+                    limits[i]=axis_start+(limits[i]/maxt)*(axis_stop-axis_start);
+                }
+                context.fillStyle=`rgba(${cl[0]},${cl[1]},${cl[2]},${OPACITY})`;
+                context.fillRect(limits[0],maxy,limits[1]-limits[0],miny-maxy);
+            }
+            context.restore();
+        }
+        
+        // Vertical Grid Lines
+        for (let t=0;t<=maxt;t+=10.0) {
+            let x=(t/maxt)*(axis_stop-axis_start)+axis_start;
+            let x2=((t+5)/maxt)*(axis_stop-axis_start)+axis_start;
+            context.beginPath();
+            context.moveTo(x,liney-0.2*AXIS);
+            context.lineTo(x,liney+0.2*AXIS);
+            context.stroke();
+            context.fillText(util.scaledround(t,2)+'s',x,liney+0.7*AXIS);
+            context.save();
+            
+            context.strokeStyle="#888888";
+            context.beginPath();
+            context.setLineDash([20,5]);
+            context.moveTo(x,liney-0.3*AXIS);
+            context.lineTo(x,this.margins[1]);
+            context.stroke();
+
+            if (t<maxt) {
+                context.strokeStyle="#666666";
+                context.beginPath();
+                context.setLineDash([20,10]);
+                context.moveTo(x2,liney);
+                context.lineTo(x2,this.margins[1]);
+                context.stroke();
+            }
+            context.restore();
+        }
+
+    }
+
+    
+    addItemToFooter(footer,key) {
+
+        let bt=webutil.createbutton({
+            'name': 'Show '+key.toUpperCase(),
+            'type': 'info' ,
+            'parent' : footer,
+            'css' : {
+                'margin-right' : '20px',
+                'margin-top'  : '0px',
+            },
+        });
+        
+        bt.on('click', () => {
+            this.plotTaskDataRun(key);
+        
+        });
+    }
+
+
+    /**
+     * handles resize event from viewer
+     * @param {array} dim - [ width,height] of viewer
+     */
+    handleresize() {
+
+        if (this.resizingTimer) {
+            clearTimeout(this.resizingTimer);
+            this.resizingTimer = null;
+        }
+
+        if (this.graphWindow === null) {
+            return;
+        }
+
+        if (!this.graphWindow.isVisible()) {
+            this.createGraphWindow();
+            return;
+        }
+
+
+        this.resizingTimer = setTimeout(() => {
+            this.replotGraph(self.lastPlotFrame).catch((e) => {
+                console.log(e, e.stack);
+            });
+        }, 200);
+
+    }
+
+
+
+
 }
 
 
