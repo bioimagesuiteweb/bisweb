99445578b8ba69af5e067599e2aa67cce2b7acf1
Xenios Papademetris <xenophon.papademetris@yale.edu>
2019-07-01 16:14:14 -0400

    docker build script

--- various/docker/scripts/cmake_full.sh
+++ various/docker/scripts/cmake_full.sh
@@ -11,7 +11,6 @@ cmake -DCMAKE_TOOLCHAIN_FILE=${DIR}/compiletools/Emscripten.cmake \
       -DEigen3_DIR=${BDIR}/eigen3/share/eigen3/cmake \
       -DCMAKE_CXX_FLAGS="-o0 -s WASM=1 -s TOTAL_MEMORY=512MB" \
       -DCMAKE_EXE_LINKER_FLAGS="--pre-js ${DIR}/cpp/libbiswasm_pre.js --post-js ${DIR}/cpp/libbiswasm_post.js" \
-      -CMAKE_INSTALL_PREFIX=/opt/bisweb/out \
       -DCMAKE_VERBOSE_MAKEFILE=ON \
       -DBIS_USEGPL=ON -DBIS_GPL_DIR=/opt/bisweb/gpl \
       -DBIS_USEINDIV=ON -DIGL_DIR=/opt/bisweb/src/build/igl \
