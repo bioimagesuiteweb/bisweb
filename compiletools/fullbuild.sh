8a531c56377308bd9e33328d4ec0a65841e7b51f
Xenios Papademetris <xenophon.papademetris@yale.edu>
2019-07-02 11:22:56 -0400

    full build script update

--- compiletools/fullbuild.sh
+++ compiletools/fullbuild.sh
@@ -7,6 +7,7 @@ BDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SRCDIR="$( cd ${BDIR}/.. && pwd )"
 source ${BDIR}/emsdk_portable/emsdk_env.sh
 
+rm -rf ${BDIR}/install
 mkdir -p ${BDIR}/install
 
 echo "-----------------------------------------------------------------------"
@@ -16,7 +17,7 @@ echo "-----------------------------------------------------------------------"
 
 # Now C++ Build for WASM
 cd ${BDIR}/wasm
-
+touch CMakeCache.txt
 rm CMakeCache.txt
 cmake -DCMAKE_TOOLCHAIN_FILE=${SRCDIR}/compiletools/Emscripten.cmake \
       -DEigen3_DIR=${BDIR}/eigen3/share/eigen3/cmake \
@@ -31,42 +32,64 @@ cmake -DCMAKE_TOOLCHAIN_FILE=${SRCDIR}/compiletools/Emscripten.cmake \
 
 
 make ${BISMAKEJ} install
+cd ${BDIR}/install/bisweb
+npm pack
+mv *tgz ${BDIR}
+cd ${BDIR}
+ls -lrt *tgz
 echo "-----------------------------------------------------------------------"
-exit
+echo " Done with WASM and Command Line JS"
+echo "-----------------------------------------------------------------------"
 
+# Create server zip file
+cd ${SRCDIR}
+touch ${BDIR}/dist/a.zip
+rm ${BDIR}/dist/*zip
+
+gulp build 
+gulp zip
+mv ${BDIR}/dist/*zip ${BDIR}
 
 # Create npm package biswebbrowser
 cd ${SRCDIR}
-gulp build
 gulp npmpack
 cd ${BDIR}/dist/biswebbrowser
-pwd
-ls -l 
 npm pack
 cp *tgz ${BDIR}
+cd ${BDIR}/dist/
+ls -lrt *tgz
 
+echo "-----------------------------------------------------------------------"
+echo " Done with Web Based Tools"
+echo "-----------------------------------------------------------------------"
 
 
 echo "-----------------------------------------------------------------------"
 # Build NATIVE
 cd ${BDIR}/native
+touch CMakeCache.txt
 rm CMakeCache.txt
 
 cmake -DBIS_A_EMSCRIPTEN=OFF -DPYTHON_EXECUTABLE=`which python3` \
       -DEigen3_DIR=${BDIR}/eigen3/share/eigen3/cmake \
       -DCMAKE_VERBOSE_MAKEFILE=OFF \
       -DBIS_A_MATLAB=ON \
+      -DCMAKE_INSTALL_PREFIX=${BDIR}/install \
       -DBIS_USEGPL=ON -DBIS_GPL_DIR=${SRCDIR}/../gpl \
       -DBIS_USEINDIV=ON -DIGL_DIR=${BDIR}/igl \
       ${SRCDIR}/cpp
 ../cmake_full_native.sh
-make ${BISMAKEJ}
+make ${BISMAKEJ} install
 make package
 cp *tar.gz ${BDIR}
-
+cd {$BDIR}
+ls -lrt *tgz
 
 
 echo "-----------------------------------------------------------------------"
+echo " Done with Python/Matlab Tools"
+echo "-----------------------------------------------------------------------"
+
 # END
 cd ${BDIR}
 node ${SRCDIR}/js/bin/bisweb -h
