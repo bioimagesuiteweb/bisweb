372b8187e7ef857a58008cbdeab50cb38345614f
Zach S <ZaSaltzman@gmail.com>
2019-07-03 11:13:41 -0400

    File system and amazonaws now maintain different lists of favorites

--- js/core/bisweb_userpreferences.js
+++ js/core/bisweb_userpreferences.js
@@ -34,6 +34,7 @@ let count=1;
  * @alias biswebUserPreferences.userPreferences
  */
 
+ //TODO: Remove favoriteFolders ? 
 const userPreferences = {
     orientationOnLoad : 'None',
     snapshotscale : 2,
@@ -41,6 +42,8 @@ const userPreferences = {
     filesource : 'local',
     showwelcome : true,
     favoriteFolders : [],
+    s3Folders : [],
+    localFolders : [],
     internal : false,
     darkmode : true,
     enables3 : false,
