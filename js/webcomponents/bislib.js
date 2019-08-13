c0b2d8d132b02deb090af153d971c1b806ff22c2
Zach S <ZaSaltzman@gmail.com>
2019-08-13 11:25:04 -0400

    added base cpm element

--- js/webcomponents/bislib.js
+++ js/webcomponents/bislib.js
@@ -31,6 +31,7 @@ require('bisweb_oldgrapherelement.js');
 require('bisweb_filetreepipeline.js');
 require('bisweb_keylistener.js');
 require('bisweb_popoverhandler.js');
+require('bisweb_cpmelement.js');
 
 
 const webutil = require('bis_webutil');
---
+++ js/webcomponents/bisweb_cpmelement.js
@@ -0,0 +1,48 @@
+/*  LICENSE
+ 
+ _This file is Copyright 2018 by the Image Processing and Analysis Group (BioImage Suite Team). Dept. of Radiology & Biomedical Imaging, Yale School of Medicine._
+ 
+ BioImage Suite Web is licensed under the Apache License, Version 2.0 (the "License");
+ 
+ - you may not use this software except in compliance with the License.
+ - You may obtain a copy of the License at [http://www.apache.org/licenses/LICENSE-2.0](http://www.apache.org/licenses/LICENSE-2.0)
+ 
+ __Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.__
+ 
+ ENDLICENSE */
+
+/*global window,document,setTimeout,HTMLElement */
+
+"use strict";
+
+const bis_webutil = require('bis_webutil.js');
+const moduleIndex = require('moduleindex.js');
+
+const connmatrixModule = moduleIndex.getModule('makeconnmatrixfile');
+class CPMElement extends HTMLElement {
+
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.menubarid = this.getAttribute('bis-menubarid');
+        bis_webutil.runAfterAllLoaded( () => {
+            let menubar = document.querySelector(this.menubarid).getMenuBar();
+            this.createMenubarItems(menubar);
+        });
+    }
+
+    createMenubarItems(menubar) {
+        let topmenu = bis_webutil.createTopMenuBarMenu('CPM', menubar);
+        bis_webutil.createMenuItem(topmenu, 'Open Connectivity File Loader');
+
+    }
+}
+
+bis_webutil.defineElement('bisweb-cpmelement', CPMElement);
+
