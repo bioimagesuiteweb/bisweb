15d68bff7d14a1bb39fd18d6324ee091e26ce445
Xenios Papademetris <xenophon.papademetris@yale.edu>
2019-08-23 11:28:43 -0400

    update

--- docker/dockerrun.sh
+++ docker/dockerrun.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-docker run  -it  -p 8080:8080 -p 24000:24000 \
+DIR=${HOME}
+USERID=`id -u $USER`
+
+echo "Starting bisweb/devel container with external persistent directory ${DIR} and uid=${USERID}"
+
+docker run --rm  -it  -p 8080:8080 -p 24000:24000 \
+     --mount src=${DIR},target=/hostfiles,type=bind \
+     -e LOCAL_USER_ID=${USERID} \
      --name bisweb \
      bisweb/devel 
 
