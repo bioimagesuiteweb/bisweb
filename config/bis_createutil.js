e80455755d656a1c0d4ff7cc693d0999970072c4
Xenios Papademetris <xenophon.papademetris@yale.edu>
2019-09-11 20:47:45 -0400

    improvements to installer

--- config/bis_createutil.js
+++ config/bis_createutil.js
@@ -1,7 +1,7 @@
 const fs=require('fs');
 const path=require('path');
 const child_process=require('child_process');
-
+const unzip=require('unzip');
 
 const makeDir=function(f1,exit=true) {
     console.log('++++ creating directory',f1);
@@ -34,14 +34,20 @@ const copyFileSync=function(d1,fname,d2,fname2) {
     }
 };
 
-let executeCommand=function(command,dir,force=false) {
+let executeCommand=function(command,dir,force=false,dummy=false) {
 
     let done=false;
     
     if (force)
         console.log('++++ The next step will take a while ...');
 
+    if (dummy) {
+        console.log(command);
+        return;
+    }
+    
     console.log('++++ ['+dir+"] "+command);
+
     
     return new Promise( (resolve,reject) => {
 
@@ -129,9 +135,31 @@ let initialize=function(DIR,exit=true) {
     copyFileSync2(WASMDIR,`biswasmdate.js`,DIR,`wasm`);
 };
 
+let unzipf=function(zipfile,outdir) {
+
+    console.log('++++ unzipping',zipfile,'in',outdir);
+    
+    return new Promise( (resolve,reject) => {
+        fs.createReadStream(zipfile).
+            pipe(unzip.Extract({ path: outdir })).
+            on('close' , () => {
+                console.log('++++ done unzip ',zipfile);
+                resolve();
+            }).
+            on('error', (e) => {
+                console.log('++++ error unzip ',zipfile);
+                reject(e);
+            });
+    });
+};
+              
+
+
+
 module.exports = {
     makeDir : makeDir,
     copyFileSync : copyFileSync,
     executeCommand : executeCommand,
+    unzip : unzipf,
     initialize : initialize,
 };
