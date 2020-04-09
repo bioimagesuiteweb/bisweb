b15c87881f8838ad6bba081e4912d058e548ac8a
Xenophon Papademetris <xenophon.papademetris@yale.edu>
2020-04-09 17:56:21 -0400

    tweaks to tests

--- actions/test/entrypointtest.sh
+++ actions/test/entrypointtest.sh
@@ -32,34 +32,22 @@ sleep 2
 
 
 # ------------------------------------------------------
-BASE = $1
+BASE=/basedir
 
-if  [ -z ${BASE}]; then
-    BASE=/basedir
-
-    if [ -d  /hostfiles ]; then
-        # Running in persisent container, no need to build
-        BASE=/hostfiles/biswebcontainer
-    else
-        # New container configure everything
-        mkdir ${BASE}
-        echo "+++ Creating base directory inside the container in ${BASE}"
-        cd ${BASE}
-
-        export PATH=/usr/local/bin:${PATH}
-        cd ${BASE}
-        mkdir -p bisweb
-        cd bisweb
-        /usr/local/bin/biswebconfig.sh
-    fi
+if [ -d  /hostfiles ]; then
+    # Running in persisent container, no need to build
+    BASE=/hostfiles/biswebcontainer
 else
-    mkdir -p ${BASE}
+    # New container configure everything
+    mkdir ${BASE}
     echo "+++ Creating base directory inside the container in ${BASE}"
+    cd ${BASE}
+    
+    export PATH=/usr/local/bin:${PATH}
     cd ${BASE}
     mkdir -p bisweb
     cd bisweb
-    chmod +x ${BASE}/biswebconfig.sh
-    ${BASE}/biswebconfig.sh
+    /usr/local/bin/biswebconfig.sh
 fi
 
 BDIR=${BASE}/bisweb/src/build
