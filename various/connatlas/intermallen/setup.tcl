e170bee197df81ac2e699f238443a00a1903447a
Xenophon Papademetris <xenophon.papademetris@yale.edu>
2020-08-11 15:02:10 -0400

    update

--- various/connatlas/intermallen/setup.tcl
+++ various/connatlas/intermallen/setup.tcl
@@ -1,9 +1,9 @@
  
-puts stdout "parsing setup for 368 atlas"
+puts stdout "parsing setup for 60 atlas"
 
-set surface_names { "allen_right_80.vtk"  "allen_left_80.vtk" }
-set atlas_names {  "allen_right_80.vtk" "allen_left_80.vtk" }
-set output_names { "output/right_allen_80" "output/left_allen_80" }
-set output_offsets { 1 82 }
+set surface_names { "allen_right_60.vtk"  "allen_left_60.vtk" }
+set atlas_names {  "allen_right_60.vtk" "allen_left_60.vtk" }
+set output_names { "output/right_allen_60" "output/left_allen_60" }
+set output_offsets { 1 60 }
 set surface_points_ras 1
 set distance_threshold 36.0
