b1105394c65dcde3bf90041825908970afbc9415
DaveOC90 <6143678+DaveOC90@users.noreply.github.com>
2021-06-17 15:59:44 -0400

    making resize image function return numpy array instead of PIL image object

--- biswebpython/utilities/calcium_image.py
+++ biswebpython/utilities/calcium_image.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import numpy as np
+import pdb
 
 def rotate(img,angle):
     rotatedArray = np.empty((Image.fromarray(img[:,:,0]).rotate(angle,Image.NEAREST,True).size)+(img.shape[-1],))
@@ -8,13 +9,16 @@ def rotate(img,angle):
     return rotatedArray
 
 def resize(img,newSize):
+    
     if len(img.shape)==3: #3d
-        resizedArray = np.empty((newSize[1],newSize[0],img.shape[2]))
+        resizedArray = np.empty((newSize[0],newSize[1],img.shape[2]))
         for i in range(img.shape[-1]):
-            resizedArray[:,:,i] = Image.fromarray(img[:,:,i]).resize(newSize,Image.BILINEAR)
+            resizedArray[:,:,i] = Image.fromarray(img[:,:,i]).resize([newSize[1],newSize[0]],Image.BILINEAR)
     else: #2d
-        resizedArray = np.empty((newSize[1],newSize[0]))
-        resizedArray = Image.fromarray(img).resize(newSize,Image.BILINEAR)
+        resizedArray = np.empty((newSize[0],newSize[1]))
+        resizedArray[:,:] = Image.fromarray(img).resize([newSize[1],newSize[0]],Image.BILINEAR)
+
+
     return resizedArray
 
 def channelSeparate(img):
