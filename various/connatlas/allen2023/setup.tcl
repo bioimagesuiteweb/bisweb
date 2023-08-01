eab85041c2dde6855c740f1775bc7a092e1b7037
Xenios Papademetris <xenophon.papademetris@yale.edu>
2023-08-01 16:42:32 -0400

    Allen 172 part 1

---
+++ various/connatlas/allen2023/setup.tcl
@@ -0,0 +1,9 @@
+
+puts stdout "parsing setup for 368 atlas"
+
+set surface_names { "2023_right.vtk"  "2023_left.vtk" }
+set atlas_names {  "2023_right.vtk" "2023_left.vtk" }
+set output_names { "output/right_allen_172" "output/left_allen_172" }
+set output_offsets { 1 1 }
+set surface_points_ras 1
+set distance_threshold 36.0
