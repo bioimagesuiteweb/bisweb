f0fa1c17a82dc86c8a1573d79e14a6cf51924b77
Xenios Papademetris <xenophon.papademetris@yale.edu>
2026-03-31 14:47:04 -0400

    update

---
+++ various/connatlas/fineallen/setup.tcl
@@ -0,0 +1,9 @@
+
+puts stdout "parsing setup for fine allen atlas"
+
+set surface_names { "allen_right_224.vtk"  "allen_left_224.vtk" }
+set atlas_names {  "allen_right_224.vtk" "allen_left_224.vtk" }
+set output_names { "output/right_allen_224" "output/left_allen_224" }
+set output_offsets { 1 224 }
+set surface_points_ras 1
+set distance_threshold 36.0
